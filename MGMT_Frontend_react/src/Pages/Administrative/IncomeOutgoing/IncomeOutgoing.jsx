b33c227302e08fc3494c3d9e19300c21d0a6a638
Swati-Ascentech <swati.gupta@ascentechindia.com>
2026-08-01 10:37:34 +0530

    swati

--- MGMT_Frontend_react/src/Pages/Administrative/IncomeOutgoing/IncomeOutgoing.jsx
+++ MGMT_Frontend_react/src/Pages/Administrative/IncomeOutgoing/IncomeOutgoing.jsx
@@ -25,7 +25,7 @@ export default function IncomeOutgoing() {
   const navigate = useNavigate();
   const [query, setQuery] = useState("");
   const handleGoBack = () => {
-    navigate("/Administrative");
+    navigate("/home");
   };
 
   const filteredTiles = tilesData.filter((t) =>
